6d332767cd909a28c6fcb9bf153e4b2b18c6f526
Mohyminul Islam <mohyminulmunna23@gmail.com>
2025-09-15 21:31:29 +0600

    I just practice Onclick Function & addEventistener

---
+++ oneClick-addEventListener/app.js
@@ -0,0 +1,9 @@
+//We will try :-  onclick handler & addEventListener
+
+// onclick function  'green and red colour'
+function makeGreen(){
+        document.body.style.backgroundColor = 'green';
+    } 
+function makeRed(){
+    document.body.style.backgroundColor = 'red';
+}
