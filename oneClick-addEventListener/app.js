72a25148396dde8ecd5e08204a7fa5b8f2f6c1f5
Mohyminul Islam <mohyminulmunna23@gmail.com>
2025-09-15 21:50:53 +0600

    practice addEventistener with color code

--- oneClick-addEventListener/app.js
+++ oneClick-addEventListener/app.js
@@ -7,3 +7,16 @@ function makeGreen(){
 function makeRed(){
     document.body.style.backgroundColor = 'red';
 }
+
+// addEventListener
+document.getElementById('purple').addEventListener('click', function makePurple(){
+    document.body.style.backgroundColor = ('purple');
+});
+
+document.getElementById('yellow').addEventListener('click', function makeYellow(){
+    document.body.style.backgroundColor = ('Yellow');
+});
+// use color code 
+document.getElementById('gold').addEventListener('click', function makeGold(){
+    document.body.style.backgroundColor = ('#A9A9A9')
+});
