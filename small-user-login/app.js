34820bbf757a9f656f4afa89e23d38f20feb5c80
Mohyminul Islam <mohyminulmunna23@gmail.com>
2025-09-15 22:31:27 +0600

    when btn click the text will be changed

---
+++ small-user-login/app.js
@@ -0,0 +1,4 @@
+document.getElementById('click-update').addEventListener('click', function () {
+    const pageTileChanged = document.getElementById('hello-user');
+    pageTileChanged.innerText = 'This title is update!'
+});
