526fab1f1cff3b1fc3b6435662b7ebb6eacdb83b
Mohyminul Islam <mohyminulmunna23@gmail.com>
2025-09-15 22:39:25 +0600

    user login

--- small-user-login/app.js
+++ small-user-login/app.js
@@ -2,3 +2,8 @@ document.getElementById('click-update').addEventListener('click', function () {
     const pageTileChanged = document.getElementById('hello-user');
     pageTileChanged.innerText = 'This title is update!'
 });
+
+document.getElementById('login-btn').addEventListener('click', function(){
+    const userNameChanged = document.getElementById('user-title');
+    userNameChanged.innerText = 'Congratulation, login successful'
+});
